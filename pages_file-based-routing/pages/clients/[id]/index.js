f0312e468d5793d348cd3468bf30254283f8a07e
tiedsandi <fachransandi@gmail.com>
2024-03-05 09:02:28 +0700

    navigation programmatically and adding custom 404 page

--- pages_file-based-routing/pages/clients/[id]/index.js
+++ pages_file-based-routing/pages/clients/[id]/index.js
@@ -3,9 +3,15 @@ import {useRouter} from 'next/router';
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push('/clients/max/project-a');
+  }
+
   return (
     <div>
       <h1>the projects of a given clinet</h1>
+      <button onClick={loadProjectHandler}>Load project a</button>
     </div>
   );
 }
