06e5be439747de04d2f1d9b12bedf53de4d56a5b
tiedsandi <fachransandi@gmail.com>
2024-03-05 08:50:12 +0700

    navigation using Link components and navigate to dynamic link

--- pages_file-based-routing/pages/clients/index.js
+++ pages_file-based-routing/pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+  const clients = [
+    {id: 'sandi', name: 'Sandi'},
+    {id: 'fachran', name: 'Fachran'},
+  ];
   return (
     <div>
       <h1>the clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
