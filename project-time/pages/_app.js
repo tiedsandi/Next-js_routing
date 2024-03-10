ccdce8f2ef435eaa3dfa39419b24a7c863ec1d96
tiedsandi <fachransandi@gmail.com>
2024-03-10 15:02:31 +0700

    add layout nav bar and logo

--- project-time/pages/_app.js
+++ project-time/pages/_app.js
@@ -1,11 +1,14 @@
 import CustomHead from '../components/custom-head';
+import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({Component, pageProps}) {
   return (
     <>
       <CustomHead pageTitle='My Project' description='This is the home page' keywords='home, page' />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
