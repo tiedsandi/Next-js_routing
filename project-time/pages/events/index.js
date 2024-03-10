e6270f20da8d920d99e59971579323dbdd4128e3
tiedsandi <fachransandi@gmail.com>
2024-03-10 20:42:39 +0700

    add all events page

--- project-time/pages/events/index.js
+++ project-time/pages/events/index.js
@@ -1,7 +1,12 @@
+import EventList from '../../components/events/event-list';
+import {getAllEvents} from '../../dummy-data';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>AllEventsPage</h1>
+      <EventList items={events} />
     </div>
   );
 }
